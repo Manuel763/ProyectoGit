20967a1793c030d579553737a0cf58ed0a83ecde
Manuel <manuelsarabiaribagorda@gmail.com>
2021-03-28 21:43:24 +0200

    Se pide el nombre y apellidos al usuario

--- ProyectoGit/src/Carpeta/Main.java
+++ ProyectoGit/src/Carpeta/Main.java
@@ -1,9 +1,16 @@
 package Carpeta;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Primer proyecto con GIT");
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Nombre y apellidos: ");
+		String usuario = sc.next();
+		sc.nextLine();
 
 	}
 
